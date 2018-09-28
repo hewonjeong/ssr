c289850889ac609fa567efec7dc27ba622680050
Hewon Jeong <hewon.jng@gmail.com>
2018-09-28 20:36:12 +0900

    feat: add head tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+
+export const HeadComponent = () => (
+  <Head>
+    <title>SSR</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
+);
+
 const Index = ({ title = 'Testing SSR🤓' }) => (
   <div>
+    <HeadComponent />
     <h2>{title}</h2>
   </div>
 );
