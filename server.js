a08a9b3b8de0895dceff47008565eb3fe688676a
Hewon Jeong <hewon.jng@gmail.com>
2018-10-14 13:05:41 +0900

    refactoring: refactor server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const next = require('next');
 
-const DEAFULT_PORT = 3800;
+const PORT = 3800;
 
 const dev = process.env.NODE_ENV !== 'production';
-const next = next({ dev });
-const handle = next.getRequestHandler();
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
-next.prepare().then(() => {
+nextApp.prepare().then(() => {
   const server = express();
 
-  server.get('*', (req, res) => handle(req, res));
+  server.get('*', handle);
 
-  server.listen(DEAFULT_PORT, err => {
+  server.listen(PORT, err => {
     if (err) throw err;
-    dev && console.log(`> Ready on http://localhost:${port}`);
+    dev && console.log(`> Ready on http://localhost:${PORT}`);
   });
 });
